48bd53da115f296a0a231769f6b66aaa77f18796
Connor Malley <connormalley98@gmail.com>
2019-05-12 19:23:55 -0400

    first edit of helloworld

---
+++ helloworld.py
@@ -0,0 +1,14 @@
+class helloworld(object):
+	x = 0
+	y = 0
+	
+	def __init__(self, x, y):
+		self.x = x
+		self.y = y
+
+	def __str__(self):
+		return "Hello, world! : x = %d y = %d \n\n" % (self.x, self.y)
+
+
+bop = helloworld(6, 9)
+for i in range(0, 10):
