7cda4313686afad007051bcbf29e4bb9901ad876
Connor Malley <connormalley98@gmail.com>
2019-05-12 20:31:23 -0400

    test123

---
+++ helloworld.cpp
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <iostream>
+#include <string.h>
+
+using namespace std;
+
+int main(void)
+{
+	string str;
+	cin >> str;
+	cout << str << endl;
+}
+
